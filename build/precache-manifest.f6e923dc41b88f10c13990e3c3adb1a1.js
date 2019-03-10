27c26722202e61cda192a17f2a972177745c7e83
ineedthisforCPEN <mihailo@shaw.ca>
2019-03-09 19:33:30 -0800

    Adding build files

---
+++ build/precache-manifest.f6e923dc41b88f10c13990e3c3adb1a1.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "912ef541256ecd4e0d2ebc6a5c2e2826",
+    "url": "/static/media/team_chackwell.912ef541.gif"
+  },
+  {
+    "revision": "09017e93ad3b55f58a3b",
+    "url": "/static/css/main.ff38ea05.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7e423d2e3375d92406cb",
+    "url": "/static/js/2.7e423d2e.chunk.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "f65ce45a014b8ee1c1ab0c36de1fbbf9",
+    "url": "/static/media/team_ahong.f65ce45a.gif"
+  },
+  {
+    "revision": "09017e93ad3b55f58a3b",
+    "url": "/static/js/main.09017e93.chunk.js"
+  },
+  {
+    "revision": "17da370438445c1bec8d5a171005567e",
+    "url": "/static/media/team_drulofs.17da3704.gif"
+  },
+  {
+    "revision": "d5e73888c019d3f6f260fe7bdab7dfee",
+    "url": "/static/media/team_lmarcil.d5e73888.gif"
+  },
+  {
+    "revision": "1d80020263aa728984178e11bcfc0a81",
+    "url": "/static/media/team_mstefanovic.1d800202.gif"
+  },
+  {
+    "revision": "a6b3cd9facbe52c924a979da4874df09",
+    "url": "/static/media/team_rliu.a6b3cd9f.gif"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "199e9ec14308832c6a8a49bd96c1fde6",
+    "url": "/static/media/bg4.199e9ec1.jpg"
+  },
+  {
+    "revision": "4035af3388ef9225ec1e826a94817ef1",
+    "url": "/static/media/banner_busymall.4035af33.jpeg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "b764b017116afbf2fb4db41f2541bf1b",
+    "url": "/index.html"
+  }
+];
