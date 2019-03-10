5b22de3e56c84e9ff9acd90e3d1137644994c655
ineedthisforCPEN <mihailo@shaw.ca>
2019-03-10 15:44:52 -0700

    Adding sign-up page - incomplete

--- src/assets/jss/material-kit-react/views/signupPage.jsx
+++ src/assets/jss/material-kit-react/views/signupPage.jsx
@@ -1,6 +1,6 @@
 import { container } from "assets/jss/material-kit-react.jsx";
 
-const loginPageStyle = {
+const signupPageStyle = {
   container: {
     ...container,
     zIndex: "2",
@@ -55,19 +55,36 @@ const loginPageStyle = {
     padding: "20px 0",
     marginBottom: "15px"
   },
+  socialIcons: {
+    maxWidth: "24px",
+    marginTop: "0",
+    width: "100%",
+    transform: "none",
+    left: "0",
+    top: "0",
+    height: "100%",
+    lineHeight: "41px",
+    fontSize: "20px"
+  },
+  divider: {
+    marginTop: "30px",
+    marginBottom: "0px",
+    textAlign: "center"
+  },
   cardFooter: {
     paddingTop: "0rem",
     border: "0",
     borderRadius: "6px",
     justifyContent: "center !important"
   },
+  socialLine: {
+    marginTop: "1rem",
+    textAlign: "center",
+    padding: "0"
+  },
   inputIconsColor: {
     color: "#495057"
-  },
-  login: {
-    display: "flex",
-    justifyContent: "center"
   }
 };
 
-export default loginPageStyle;
+export default signupPageStyle;
