c3c4edd09886307b895b080dcd20b8e4f445ea6c
ineedthisforCPEN <mihailo@shaw.ca>
2019-03-12 20:44:42 -0700

    Error checking inputs on sign up page
    Not fully tested, but password field should be good

--- src/views/SignupPage/SignupPage.jsx
+++ src/views/SignupPage/SignupPage.jsx
@@ -22,13 +22,42 @@ import CustomInput from "components/CustomInput/CustomInput.jsx";
 import signupPageStyle from "assets/jss/material-kit-react/views/signupPage.jsx";
 
 import image from "assets/img/banner_busymall.jpeg";
+import { TextField } from "@material-ui/core";
+import { relativeTimeThreshold } from "moment";
+
+const renderTextField = ({
+  input,
+  label,
+  meta: {touched, error},
+  ...custom
+}) => (
+  <TextField
+    hintText={label}
+    floatingLabelText={label}
+    errorText={touched && error}
+    {...input}
+    {...custom}
+  />
+)
 
 class SignupPage extends React.Component {
   constructor(props) {
     super(props);
     // we use this to make the card to appear after the page has been rendered
     this.state = {
-      cardAnimaton: "cardHidden"
+      cardAnimaton: "cardHidden",
+      form: {
+        name: "",
+        parent: "",
+        email: "",
+        password: "",
+        confirm: ""
+      },
+      errors: {
+        email: false,
+        password: false,
+        confirm: false
+      }
     };
   }
 
@@ -44,6 +73,51 @@ class SignupPage extends React.Component {
 
   redirectLogin = () => {
     this.props.history.push("/login");
+  }
+
+  onBlur(event) {
+    const target = event.target;
+    const form = this.state.form;
+    const errors = this.state.errors;
+    form[target.id] = target.value;
+
+    // Perform error checking
+    if (target.id === "email") {
+      errors.email = !this.validEmail(form.email);
+    } else if (target.id === "password") {
+      errors.password = !this.validPassword(form.name, form.password);
+      if (form.confirm) {
+        errors.confirm = (form.password !== form.confirm);
+      }
+    } else if (target.id === "confirm") {
+      errors.confirm = (form.password !== form.confirm);
+    }
+    // console.log(form);
+    console.log(errors.password);
+  }
+
+  handleInputChange = (event) => {
+    console.log(this.state.form.name);
+  }
+
+  validEmail(email) {
+    const rfc5322 = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const res = email.match(rfc5322);
+    return (res !== null);
+  }
+
+  validPassword(username, password) {
+    const passwordUpper = password.toUpperCase();
+    const usernameUpper = username.toUpperCase();
+
+    // Password cannot contain username
+    if (usernameUpper && passwordUpper.includes(usernameUpper)) {
+      return false;
+    }
+
+    // Require a number, symbol, and alphanumeric characters
+    const matcher = new RegExp("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])(?=.{8,512})");
+    return matcher.test(password);
   }
 
   render() {
@@ -77,6 +151,7 @@ class SignupPage extends React.Component {
                       <CustomInput
                         labelText="Store Name"
                         id="name"
+                        onBlur={this.onBlur.bind(this)}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -84,6 +159,7 @@ class SignupPage extends React.Component {
                       <CustomInput
                         labelText="Parent Company (optional)"
                         id="parent"
+                        onBlur={this.onBlur.bind(this)}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -92,6 +168,7 @@ class SignupPage extends React.Component {
                       <CustomInput
                         labelText="Email"
                         id="email"
+                        onBlur={this.onBlur.bind(this)}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -106,7 +183,8 @@ class SignupPage extends React.Component {
                       />
                       <CustomInput
                         labelText="Password"
-                        id="pass"
+                        id="password"
+                        onBlur={this.onBlur.bind(this)}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -123,7 +201,8 @@ class SignupPage extends React.Component {
                       />
                       <CustomInput
                         labelText="Confirm Password"
-                        id="pass_confirm"
+                        id="confirm"
+                        onBlur={this.onBlur.bind(this)}
                         formControlProps={{
                           fullWidth: true
                         }}
@@ -159,8 +238,8 @@ class SignupPage extends React.Component {
               </GridItem>
             </GridContainer>
           </div>
-          <Footer whiteFont />
         </div>
+        <Footer />
       </div>
     );
   }
