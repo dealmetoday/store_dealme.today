01a3cd42b7695689048240eb0ae3a8aafc29f674
ineedthisforCPEN <mihailo@shaw.ca>
2019-03-20 19:21:22 -0700

    No longer using Redirect - removed it from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import "assets/scss/material-kit-react.scss?v=1.4.0";
 import "assets/css/material-dashboard-react.css?v=1.6.0";
