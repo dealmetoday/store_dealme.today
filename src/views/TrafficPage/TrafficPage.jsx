0fb20838612807fd24bcb174a10a5cb6487e4fb4
ineedthisforCPEN <mihailo@shaw.ca>
2019-03-21 11:57:49 -0700

    Small update to traffic page

--- src/views/TrafficPage/TrafficPage.jsx
+++ src/views/TrafficPage/TrafficPage.jsx
@@ -4,13 +4,17 @@ import PropTypes from "prop-types";
 // @material-ui/core
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardIcon from "components/Card/CardIcon.jsx";
+import CardBody from "components/Card/CardBody.jsx";
 import DashboardHeader from "components/Header/DashboardHeader.jsx";
 import DashboardHeaderLinks from "components/Header/DashboardHeaderLinks.jsx";
-// core components
 import GridItem from "components/Grid/GridItem.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
 
 import dashboardStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+import Accessibility from "@material-ui/icons/Accessibility";
 
 const dashboardRoutes = [];
 
@@ -37,6 +41,34 @@ class TrafficPage extends React.Component {
         <br/><br/><br/><br/><br/>
         <GridContainer justify="center">
           <GridItem xs={11} sm={10} md={10}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <Card>
+                  <CardHeader color="success" stats icon>
+                    <CardIcon color="success">
+                      <Accessibility />
+                    </CardIcon>
+                    <h3 className={classes.cardTitleWhite}>Traffic Breakdown</h3>
+                  </CardHeader>
+                  <CardBody>
+                    <p>LEFT</p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={6}>
+              <Card>
+                  <CardHeader color="success" stats icon>
+                    <CardIcon color="success">
+                      <Accessibility />
+                    </CardIcon>
+                    <h3 className={classes.cardTitleWhite}>Traffic Timeline</h3>
+                  </CardHeader>
+                  <CardBody>
+                    <p>RIGHT</p>
+                  </CardBody>
+                </Card>
+              </GridItem>
+            </GridContainer>
           </GridItem>
         </GridContainer>
       </div>
